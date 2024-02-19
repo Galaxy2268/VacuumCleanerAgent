40615f2031c2cbe171013b2990228c3e6386800c
Galaxy2268 <danielsmulukinss@gmail.com>
2024-02-19 17:14:46 +0200

    Added Pair class and implemented it, also made sample tests

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "Vector/Vector.h"
 #include "List/List.h"
+#include "Pair/Pair.h"
 
 using namespace std;
 
@@ -10,12 +11,19 @@ using namespace std;
 //Main function
 int main(){
 
-    List<int> list(1,2,3,4,5,6,7,8);
-    list.reverse();
-    list.print();
-    list.reverse();
-    list.print();
-    list.reverse();
-    list.print();
+    Pair<string, int> pair("A", 75);
+    Pair<string, int> pair1("B", 125);
+    Pair<string, int> pair2("C", 10);
+
+    cout << pair1.getFirst() << "\n";
+    cout << pair1.getSecond()<< "\n";
+    pair1.setFirst("B1");
+    pair1.setSecond(130);
+    cout << pair1.getFirst()<< "\n";
+    cout << pair1.getSecond()<< "\n";
+
+    cout << "TEST" << "\n";
+    pair1.print();
+
 
 }
---
+++ Pair/Pair.h
@@ -0,0 +1,29 @@
+#ifndef PAIR_H
+#define PAIR_H
+
+template <typename T1, typename T2>
+class Pair{
+
+private:
+T1 first;
+T2 second;
+
+
+public:
+Pair(T1 first, T2 second);
+
+T1 getFirst();
+
+T2 getSecond();
+
+void setFirst(T1 first);
+
+void setSecond(T2 second);
+
+void print();
+
+
+};
+
+#include "Pair.cpp"
+#endif
