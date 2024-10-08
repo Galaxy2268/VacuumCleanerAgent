fe8fefee3d29f63229a9a273c367d3ef37d2809e
Galaxy2268 <danielsmulukinss@gmail.com>
2024-10-08 14:49:08 +0300

    replaced comment in header file

--- List/List.h
+++ List/List.h
@@ -6,79 +6,95 @@ template <class T>
 class List{
 
 private:
-ListEl<T> * head;
-ListEl<T> * tail;
-int length{};
+    ListEl<T> * head;
+    ListEl<T> * tail;
+    int length;
 
-/*
-Description:
- Palīg funkcija, kura meklē elementu, izmantojot indeksu, no saraksta gala, lai izdzēstu to.
- Helper function which searches element by id from end of the list to delete it.
+    /*
+    Description:
+     Palīg funkcija, kura meklē elementu, izmantojot indeksu, no saraksta gala, lai izdzēstu to.
+     Helper function which searches element by id from end of the list to delete it.
 
-Complexety:
- Labākajā gadijumā - O(1)
- Sliktākajā gadijumā - O(n)
- Atkarībā no elementa atrašanas vietas.
+    Complexety:
+     Labākajā gadijumā - O(1)
+     Sliktākajā gadijumā - O(n)
+     Atkarībā no elementa atrašanas vietas.
 
- In best case - O(1)
- In worst case - O(n)
- Depends on element id.
-*/
-void searchDeleteBack(int id);
+     In best case - O(1)
+     In worst case - O(n)
+     Depends on element id.
+    */
+    void searchDeleteBack(int id);
 
-void searchDeleteFront(int id);
 
-ListEl<T> * split(ListEl<T> * head);
+    /*
+    Description:
+     Palīg funkcija, kura meklē elementu, izmantojot indeksu, no saraksta sākuma, lai izdzēstu to.
+     Helper function which searches element by id from start of the list to delete it.
 
-ListEl<T> * merge(ListEl<T> *first, ListEl<T> *second);
+    Complexety:
+     Labākajā gadijumā - O(1)
+     Sliktākajā gadijumā - O(n)
+     Atkarībā no elementa atrašanas vietas.
 
-ListEl<T> * mergeSort(ListEl<T> * head);
+     In best case - O(1)
+     In worst case - O(n)
+     Depends on element id.
+    */
+    void searchDeleteFront(int id);
+
+
+    ListEl<T> * split(ListEl<T> * head);
+
+    ListEl<T> * merge(ListEl<T> *first, ListEl<T> *second);
+
+    ListEl<T> * mergeSort(ListEl<T> * head);
 
 public:
 
-template <typename... Args>
-List(Args... data);
+    template <typename... Args>
+    List(Args... data);
 
-~List();
+    ~List();
 
-void sort();
+    void sort();
 
 //use if your list is already sorted
-void addSortedBack(T data);
+    void addSortedBack(T data);
 
-void clear();
+    void clear();
 
-void reverse();
+    void reverse();
 
-int getIdOf(T data);
+    int getIdOf(T data);
 
-T getElById(int index);
+    T getElById(int index);
 
-T& getReferenceById(int index);
+    T& getReferenceById(int index);
 
-T getFirst();
+    T getFirst();
 
-T getLast();
+    T getLast();
 
-bool isEmpty();
+    bool isEmpty();
 
-int size();
+    int size();
 
-void addBack(T data);
+    void addBack(T data);
 
-void addFront(T data);
+    void addFront(T data);
 
-void add(T data, int index);
+    void add(T data, int index);
 
-void print();
+    void print();
 
-void remove(T data);
+    void remove(T data);
 
-void removeBack();
+    void removeBack();
 
-void removeFront();
+    void removeFront();
 
-void removeById(int id);
+    void removeById(int id);
 
 };
 
